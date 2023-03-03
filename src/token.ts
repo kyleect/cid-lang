5d9e616d6a4051a5f9c14d5ee73691bf8d393126
Kylee Tilley <kyleetilley@gmail.com>
2023-03-02 23:15:02 -0600

    Add token type enum

---
+++ src/token.ts
@@ -0,0 +1,9 @@
+export enum TokenType {
+  LeftBracket = "LeftBracket",
+  RightBracket = "RightBracket",
+  Symbol = "Symbol",
+  Number = "Number",
+  Boolean = "Boolean",
+  String = "String",
+  Eof = "Eof",
+}
