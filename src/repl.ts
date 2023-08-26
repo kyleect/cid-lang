874d37e68fe0d16bfd1a8e0e6cc4db5c7f80b71f
Kylee Tilley <kyleetilley@gmail.com>
2023-08-26 09:34:03 +0000

    Update reply to use exec instead of interpreter

--- src/repl.ts
+++ src/repl.ts
@@ -1,7 +1,5 @@
 import repl from "repl";
-import { Interpreter } from "./interpreter";
-import { Parser } from "./parser";
-import { Scanner } from "./scanner";
+import { exec } from ".";
 
 const inputs: string[] = [];
 
@@ -9,11 +7,7 @@ function run(input: string) {
   inputs.push(input);
 
   try {
-    const scanner = new Scanner(inputs.join("\n"));
-    const parser = new Parser(scanner.scan());
-    const expressions = parser.parse();
-    const interpreter = new Interpreter();
-    return interpreter.interpretAll(expressions);
+    return exec(inputs.join("\n"));
   } catch (e) {
     inputs.pop();
     console.log(`Error with input: ${e}`);
@@ -23,15 +17,6 @@ function run(input: string) {
 repl.start({
   prompt: "schemee> ",
   eval: (input, context, filename, callback) => {
-    callback(null, stringify(run(input)));
+    callback(null, run(input));
   },
 });
-
-function stringify(value) {
-  if (value === false) return "#f";
-  if (value === true) return "#t";
-  // if (Array.isArray(value)) return "(" + value.map(stringify).join(" ") + ")";
-  if (value instanceof Function) return "function";
-  // if (typeof value === "string") return `"${value}"`;
-  return value;
-}
