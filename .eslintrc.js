c477f16b69d2a8eedb6ac5190eb6013ef216b535
Kylee Tilley <kyleetilley@gmail.com>
2023-09-12 21:02:20 +0000

    Add tsdoc and generate docs

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,12 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
   root: true,
-  ignorePatterns: ["dist/**/*.*", ".eslintrc.js", "jest.config.js"],
+  ignorePatterns: [
+    "dist/**/*.*",
+    ".eslintrc.js",
+    "jest.config.js",
+    "docs/**/*.*",
+  ],
   rules: {
     "no-unused-vars": "off",
     "@typescript-eslint/no-unused-vars": [
