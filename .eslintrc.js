d4962f182f5d587bb9f04727837b55f1afaa84c2
Kylee Tilley <kyleetilley@gmail.com>
2023-09-15 19:24:05 +0000

    Use common eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  root: true,
+  ignorePatterns: [
+    "dist/**/*.*",
+    ".eslintrc.js",
+    "jest.config.js",
+    "docs/**/*.*",
+  ],
+  rules: {
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn", // or "error"
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_",
+      },
+    ],
+  },
+};
--- packages/lib/.eslintrc.js
+++ packages/lib/.eslintrc.js
@@ -1,23 +1,3 @@
 module.exports = {
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
-  root: true,
-  ignorePatterns: [
-    "dist/**/*.*",
-    ".eslintrc.js",
-    "jest.config.js",
-    "docs/**/*.*",
-  ],
-  rules: {
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": [
-      "warn", // or "error"
-      {
-        argsIgnorePattern: "^_",
-        varsIgnorePattern: "^_",
-        caughtErrorsIgnorePattern: "^_",
-      },
-    ],
-  },
+  extends: ["../../.eslintrc.js"],
 };
